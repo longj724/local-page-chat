3e321749906a670d36153eb9cfb7ab1dc0b3209d
longj724 <justinlong596@gmail.com>
2024-06-05 23:33:26 -0400

    No messages copy

--- pages/sidepanel/src/components/ChatMessages.tsx
+++ pages/sidepanel/src/components/ChatMessages.tsx
@@ -9,13 +9,17 @@ type Props = {
 };
 
 const ChatMessages = ({ messages }: Props) => {
-  return (
+  return messages.length > 0 ? (
     <div className="flex-1 overflow-auto p-4 w-full">
       <div className="grid gap-4">
         {messages.map((message, index) => (
           <ChatMessage key={index} message={message} />
         ))}
       </div>
+    </div>
+  ) : (
+    <div className="flex flex-1 items-center">
+      <h2 className="text-base">Send a message to start chatting!</h2>
     </div>
   );
 };
